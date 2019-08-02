53fb9257d57cc2c02334223c3277928dec241d28
MarksPro <marcosfsprivado@gmail.com>
2019-08-02 13:07:20 -0300

    Refatoramento do count state do redux

--- src/components/CountTodo/index.js
+++ src/components/CountTodo/index.js
@@ -1,27 +1,22 @@
 import React from 'react';
 import {connect} from 'react-redux';
 
-const CountTodo = (props) => {
-  let todoCompleted = props.todos.filter(todo => (
-    todo.completed === true
-  )).length
-
-  let todoNotCompleted = props.todos.filter(todo => (
-    todo.completed === false
-  )).length
-  
+const CountTodo = (props) => {  
+  console.log(props)
   return (
   <div>
     <h1>Tarefas</h1>
     <div>
-      <h3>Tarefas a fazer: {todoNotCompleted} </h3>
-      <h3>Tarefas concluidas: {todoCompleted}</h3>
+      <h3>Tarefas a fazer: {props.todosCount(false)}</h3>
+      <h3>Tarefas concluidas: {props.todosCount(true)}</h3>
     </div>
   </div>
 )};
 
 const mapStateToProps = state => ({
-  todos: state.todos
+  todosCount: bool => state.todos.filter(todo => (
+    todo.completed === bool
+  )).length
 });
 
 export default connect(mapStateToProps)(CountTodo);
